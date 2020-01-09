dc46c4856959790efcad8fdb33de03f76098523b
jch9537 <jch9537@gmail.com>
2020-01-10 01:59:37 +0900

    ✨ Make Sign In/Up 로그인, 회원가입 연결완료

--- controller/user.js
+++ controller/user.js
@@ -1,10 +1,11 @@
 module.exports = {
   signup: (req, res) => {
     console.log(req.body);
+    res.status(201).send({ message: "회원가입완료" });
   },
   signin: (req, res) => {
     console.log(req.body);
-    res.status(200).send({ message: "성공" });
+    res.status(200).send({ message: "로그인완료" });
   },
   signout: (req, res) => {
     console.log(req.body);
